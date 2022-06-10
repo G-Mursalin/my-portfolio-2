2672ffd35db3a4597b463912b8525ab8b9da22b3
Golam Mursalin <golammursalin309@gmail.com>
2022-06-10 23:00:09 +0600

    first commit

---
+++ src/components/sharedPages/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <footer className="footer footer-center p-10 bg-base-300 text-base-content">
+        <div>
+          <p>
+            Copyright © {currentYear} - All right reserved by Golam Mursalin
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
